15c1cfa8db4627962069d6097b422961af3eeed6
davidebettero <davide.bettero@studenti.unimi.it>
2021-12-22 11:54:29 +0100

    Update classe Utente
    Aggiunta metodi set.

--- e-voting/src/main/java/sweng/project/evoting/Utente.java
+++ e-voting/src/main/java/sweng/project/evoting/Utente.java
@@ -28,12 +28,24 @@ public class Utente {
 		return this.username;
 	}
 	
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
 	public String getPassword() {
 		return this.password;
 	}
 	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
 	public String getTipo() {
 		return this.tipo;
+	}
+	
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
 	}
 	
 	@Override
